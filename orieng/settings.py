60a597e8a34497fbdc44711a544f298b824954a8
Br@ini@c R@wkib <brainiacrawkib@gmail.com>
2021-08-12 14:35:07 -0700

    :hammer: updated settings.py

--- orieng/settings.py
+++ orieng/settings.py
@@ -176,7 +176,7 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'orieng/static')
 ]
