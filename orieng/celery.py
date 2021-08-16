e4f0bb6b332eb85e14901841b881a5c2b47c8666
Br@ini@c R@wkib <brainiacrawkib@gmail.com>
2021-08-15 20:01:20 -0700

    :hammer: updated asynchronous functionality

--- orieng/celery.py
+++ orieng/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from django.conf import settings
 
 
 # set the default Django settings module for the celery program
@@ -9,7 +10,7 @@ app = Celery('orieng')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.conf.update(BROKER_URL=os.getenv('REDIS_TLS_URL'), CELERY_RESULT_BACKEND=os.getenv('REDIS_TLS_URL'))
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 
 @app.task(bind=True)
