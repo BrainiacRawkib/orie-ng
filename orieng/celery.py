24b53ce17180cf34100117ebc44673fdc296a47d
Br@ini@c R@wkib <brainiacrawkib@gmail.com>
2021-08-16 18:52:31 -0700

    :hammer: updated celery.py

--- orieng/celery.py
+++ orieng/celery.py
@@ -1,12 +1,13 @@
 import os
+import ssl
 from celery import Celery
 from django.conf import settings
-
 
 # set the default Django settings module for the celery program
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'orieng.settings')
 
-app = Celery('orieng', broker=os.getenv('REDIS_TLS_URL'), backend=os.getenv('REDIS_TLS_URL'))
+app = Celery('orieng', broker=os.getenv('REDIS_TLS_URL'), backend=os.getenv('REDIS_TLS_URL'),
+             redis_backend_use_ssl={'ssl_cert_reqs': ssl.CERT_NONE})
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 # app.conf.update(BROKER_URL=os.getenv('REDIS_TLS_URL'), CELERY_RESULT_BACKEND=os.getenv('REDIS_TLS_URL'))
